2f882c80d260c035104a488d57096b36471af9da
Vitaliy Pevko <vpevkoprovide@gmail.com>
2025-10-04 11:10:38 +0300

    task

---
+++ homework 2.2.py
@@ -0,0 +1,9 @@
+number = int(input("Введіть 5-ти значне число: "))
+d1 = number // 10000
+d2 = (number // 1000) % 10
+d3 = (number // 100) % 10
+d4 = (number // 10) % 10
+d5 = number % 10
+reversed_number = d5 * 10000 + d4 * 1000 + d3 * 100 + d2 * 10 + d1
+
+print("Перевернуте число:", reversed_number)
