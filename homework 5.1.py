6aae09fda0435dd022e16f3ba7cb54f622af626d
Vitaliy Pevko <vpevkoprovide@gmail.com>
2025-10-16 19:44:06 +0300

    task

--- homework 5.1.py
+++ homework 5.1.py
@@ -4,7 +4,7 @@ import keyword
 name = input("Введите имя: ").strip()
 is_valid = (
     bool(re.fullmatch(r'[a-z_][a-z0-9_]*', name)) and
-    (name == '_' or set(name) != {'_'}) and
-    not keyword.iskeyword(name)
+    (name.count('_') <= 1) and
+    (not keyword.iskeyword(name))
 )
 print(is_valid)
